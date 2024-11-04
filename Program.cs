70c48bcec00e0c1bf02f710eb3f2023329d88883
John DeVries <johndevries49@gmail.com>
2024-11-04 13:18:17 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment1_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            double myAge = Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine("Please enter your address: ");
+            string myAddress = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("My name is "+myName);
+            Console.WriteLine("My age is "+myAge);
+            Console.WriteLine("My address is "+myAddress);
+        }
+    }
+}

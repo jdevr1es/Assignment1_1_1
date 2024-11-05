eb90b6e333887de6480453ba00c2c299525f0d62
John DeVries <johndevries49@gmail.com>
2024-11-05 07:30:03 -0800

    Added Exercise 2

--- Program.cs
+++ Program.cs
@@ -14,6 +14,26 @@
             Console.WriteLine("My name is "+myName);
             Console.WriteLine("My age is "+myAge);
             Console.WriteLine("My address is "+myAddress);
+
+            int intValue1 = 10;
+            int intValue2 = 20;
+
+            float fltValue1 = 5.5F;
+            float fltValue2 = 6.5F;
+
+            double doubleValue1 = 30.25;
+            double doubleValue2 = 60.25;
+
+            decimal decValue1 = 100.55M;
+            decimal decValue2 = 200.45M;
+
+            int maxInt = Math.Max(intValue1, intValue2);
+            int minInt = Math.Min(intValue1, intValue2);
+            Console.WriteLine("Max integer is :" + maxInt);
+            Console.WriteLine("Min integer is :" +  minInt);
+
+            float maxFloat = Math.Max(fltValue1, fltValue2);
+            float minFloat = Math.Min(fltValue1, fltValue2);
         }
     }
 }
